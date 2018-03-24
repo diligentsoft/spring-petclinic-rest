d59021d8e0890258dd0887bd6fa0621d4038120d
Chris Cundill <chrisc@diligentsoft.co>
2018-03-24 06:56:41 +0000

    Provided qualified path to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh -o StrictHostKeyChecking=no \
     -i /var/jenkins_home/.ssh/keys/docker-for-aws.pem \
     docker@$1 \
-    deploy.sh diligentsoft/spring-petclinic $2 spring-petclinic 9966
+    /home/docker/deploy.sh diligentsoft/spring-petclinic $2 spring-petclinic 9966
